a7e0c935bf5b6bb0038511c4aa92cf63680f6437
Ram ji <mishraramji039@gmail.com>
2025-01-21 18:24:48 +0530

    implement SavedBlogs componenet

--- src/pages/Account_page/SavedBlogs.tsx
+++ src/pages/Account_page/SavedBlogs.tsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+import { useFetchData } from "../../customHooks/useFetchData";
+import YourBlogs from "../../components/Account_details/YourBlogs";
+
+interface storiesType {
+    id: string,
+    title: string,
+    description: string,
+    image: string,
+    likeCount: number,
+    commentCount: number,
+    createdAt: string,
+}
+
 const SavedBlogs = () => {
+    const { data, error, success } = useFetchData<storiesType[]>("/api/v1/saveStories/save-stories")
+    const [stories, setStories] = useState<storiesType[]>([]);
+    useEffect(() => {
+        if (data && success) {
+            console.log("all stories fetched for current user", data);
+            setStories(data);
+        }
+    }, [data, success])
     return (
         <>
             <div className="space-y-4">
-                <h2 className="text-2xl font-semibold mb-6">Saved Stories</h2>
+                {stories.map((blog, index) => (
+                    <YourBlogs key={index} {...blog} />
+                ))}
             </div>
         </>
     )
